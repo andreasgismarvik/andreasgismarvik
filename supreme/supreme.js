2ea35ee1744fa0bb28c0702d1277bfa70558f38f
andreasgismarvik <andreasgismarvik@hotmail.com>
2018-03-06 09:41:55 +0100

    wqdqwdw

---
+++ supreme/supreme.js
@@ -0,0 +1,13 @@
+function setup() {
+
+var config = {
+    apiKey: "AIzaSyCtM_RZahKwdzmSF9AV8elR7u0NLtzYXIc",
+    authDomain: "supreme-76c54.firebaseapp.com",
+    databaseURL: "https://supreme-76c54.firebaseio.com",
+    projectId: "supreme-76c54",
+    storageBucket: "",
+    messagingSenderId: "525954289649"
+  };
+  firebase.initializeApp(config);
+
+}
